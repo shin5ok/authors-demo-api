569299102303d8d811f619798cb6a45cf463d0dc
shin5ok <shin5ok@55mp.com>
2022-06-22 23:12:16 +0900

    split routers

--- my-app.python/main.py
+++ my-app.python/main.py
@@ -1,56 +1,11 @@
-from fastapi import FastAPI, Depends, Header, Request
-from pydantic import BaseModel
-from google.cloud import firestore
+from fastapi import FastAPI
 import os
 import uvicorn
-import logging
-import json
-import time
-import sys
 
-COLLECTION: str = "authors"
+from routers import root
 
 app = FastAPI()
-db = firestore.Client()
-
-logger = logging.getLogger(__name__)
-handler = logging.StreamHandler(sys.stdout)
-logger.addHandler(handler)
-logger.setLevel(logging.DEBUG)
-class Response(BaseModel):
-    message: str
-
-class Author(BaseModel):
-    mail: str
-    username: str
-    name: str
-    address: str
-    company: str
-    job: str
-    website: list
-
-class AuthorResponse(BaseModel):
-    data: Author
-
-@app.get("/")
-def _root():
-    logger.info(f"get '/'")
-    return Response(message="Hello, World")
-
-@app.get("/api/author/{username}")
-def _author_get(username: str, request: Request, user_agent = Header(default=None), host = Header(default=None), ):
-
-    start_time = time.time()
-
-    docs = db.collection(COLLECTION).where("username", "==", username).limit(1)
-    result = docs.stream()
-    data = list(result)[-1]
-    response_data = data.to_dict()
-
-    process_time = time.time() - start_time
-
-    logger.info(json.dumps(dict(path=f"/api/author/{username}", user_agent=user_agent, host=host, process_time=process_time, remote_addr=request.client.host)))
-    return AuthorResponse(data=response_data)
+app.include_router(root.routers)
 
 if __name__ == '__main__':
     port = os.environ.get("PORT", "8080")
