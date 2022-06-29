3030d3048b72168a8e3fbf39bd7eb93f2dadf16d
shin5ok <shin5ok@55mp.com>
2022-06-30 00:35:18 +0900

    test post easily

---
+++ my-app.python/test.sh
@@ -0,0 +1,1 @@
+curl -H Content-Type: application/json localhost:8080/api/author/shingotest -d {"name":"tako","mail":"tako@example.jp","address":"","job":"","website":["https://www.google.com/"],"company":"", "username":"vdi-user"}
