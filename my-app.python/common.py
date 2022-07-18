d419ea336799bc57ccdcc1db60efd2594a92f2eb
shin5ok <shin5ok@55mp.com>
2022-07-18 22:53:50 +0900

    create dependencies

---
+++ my-app.python/common.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI, Depends, Header, Request, APIRouter
+from pydantic import BaseModel, Field, EmailStr
+from google.cloud import firestore
+import os
+import uvicorn
+import logging
+import json
+import time
+import sys
+
+COLLECTION: str = os.environ.get("COLLECTION")
+db = firestore.Client()
+
+def get_coll():
+    docs = db.collection(COLLECTION)
+    return docs
