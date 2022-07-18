68235ca05ddd96859e06413078c61ac131def12d
shin5ok <shin5ok@55mp.com>
2022-07-18 22:58:54 +0900

    declare dependencies

--- my-app.python/main.py
+++ my-app.python/main.py
@@ -1,12 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 import os
 import uvicorn
 
 from routers import root as router_root, author as router_author
 
+from common import get_coll
+
 app = FastAPI()
 app.include_router(router_root.routers)
-app.include_router(router_author.routers, prefix="/api")
+app.include_router(router_author.routers,
+                   prefix="/api",
+                   dependencies=[Depends(get_coll)]
+                   )
 
 if __name__ == '__main__':
     port = os.environ.get("PORT", "8080")
